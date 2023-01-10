816e02c8b1ca0f854c3b4b32a886d501c7ef8489
Omer Morad <omer.moradd@gmail.com>
2023-01-10 15:06:06 +0200

    feat(openapi): apply basic transactions controllers (#38)

--- openapi/packages/openapi/balance-openapi.module.ts
+++ openapi/packages/openapi/balance-openapi.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TransactionModule as TransactionModuleV1 } from './v1.0/transaction.module';
-// import { TransactionModule as TransactionModuleV1_1 } from './v1.1/transaction.module';
+import { TransactionModule as TransactionModuleV1_1 } from './v1.1/transaction.module';
 
 @Module({
-  imports: [TransactionModuleV1]
+  imports: [TransactionModuleV1, TransactionModuleV1_1]
 })
 export class BalanceOpenApiModule {}
