c7e927d3a915d2654add89e8e648a65690c9bcd0
omermorad <omer.moradd@gmail.com>
2023-01-10 13:05:10 +0200

    feat(openapi): apply basic transactions controllers (1)

--- openapi/packages/openapi/build.ts
+++ openapi/packages/openapi/build.ts
@@ -2,7 +2,7 @@
 
 import { createApiSpecFile } from '@openapi/exporter';
 import { ApplicationRootModule } from 'server/application-root.module';
-import { BalanceOpenApiModule } from '@omermorad/openapi/balance-openapi.module';
 import { version } from './package.json';
+import { TransactionModule as TransactionModuleV1_1 } from './v1.1/transaction.module';
 
-void createApiSpecFile(version, ApplicationRootModule, BalanceOpenApiModule);
+void createApiSpecFile(version, ApplicationRootModule, TransactionModuleV1_1);
