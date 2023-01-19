03ff765b6dde91ba801f0baf27757f8d2f1fc75f
omermorad <omer.moradd@gmail.com>
2023-01-19 11:13:57 +0200

    feat(openapi): create some cool feature

--- openapi/packages/openapi/openapi-versions.ts
+++ openapi/packages/openapi/openapi-versions.ts
@@ -10,9 +10,9 @@ export type OpenApiVersions = 'V1_0' | 'Next';
 
 export const OpenApiVersion: Record<OpenApiVersions, string> = {
   V1_0: '1',
-  Next: `${MAJOR_VERSION}.${Number(MINOR_VERSION) + 1}`,
+  Next: `${MAJOR_VERSION}.${MINOR_VERSION}`,
 };
 
 export type OpenApiVersion = typeof OpenApiVersion[keyof typeof OpenApiVersion];
 
-export const NEXT_OPENAPI_VERSION = `${MAJOR_VERSION}.${Number(MINOR_VERSION) + 1}`;
+export const NEXT_OPENAPI_VERSION = `${MAJOR_VERSION}.${MINOR_VERSION}`;
